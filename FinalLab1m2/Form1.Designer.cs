b3c74b7073cbcf73b5d3965047d82256889e5c46
Facundo Lopez Vital <facu_lv_13@hotmail.com>
2024-10-26 19:14:05 -0300

    Agregar archivos de proyecto.

---
+++ FinalLab1m2/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace FinalLab1m2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCap = new System.Windows.Forms.Label();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.txtDNI = new System.Windows.Forms.TextBox();
+            this.cmbSectores = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnVerTodos = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.dgvGrilla = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.cmbSectEliminar = new System.Windows.Forms.ComboBox();
+            this.txtDelete = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGrilla)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lblCap);
+            this.groupBox1.Controls.Add(this.btnReiniciar);
+            this.groupBox1.Controls.Add(this.txtDNI);
+            this.groupBox1.Controls.Add(this.cmbSectores);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnVerTodos);
+            this.groupBox1.Controls.Add(this.btnCargar);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(180, 336);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Sector";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Capacidad";
+            // 
+            // lblCap
+            // 
+            this.lblCap.AutoSize = true;
+            this.lblCap.Location = new System.Drawing.Point(76, 54);
+            this.lblCap.Name = "lblCap";
+            this.lblCap.Size = new System.Drawing.Size(26, 13);
+            this.lblCap.TabIndex = 13;
+            this.lblCap.Text = "DNI";
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Location = new System.Drawing.Point(12, 263);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(154, 47);
+            this.btnReiniciar.TabIndex = 12;
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // txtDNI
+            // 
+            this.txtDNI.Location = new System.Drawing.Point(9, 93);
+            this.txtDNI.Name = "txtDNI";
+            this.txtDNI.Size = new System.Drawing.Size(157, 20);
+            this.txtDNI.TabIndex = 11;
+            // 
+            // cmbSectores
+            // 
+            this.cmbSectores.FormattingEnabled = true;
+            this.cmbSectores.Location = new System.Drawing.Point(6, 19);
+            this.cmbSectores.Name = "cmbSectores";
+            this.cmbSectores.Size = new System.Drawing.Size(160, 21);
+            this.cmbSectores.TabIndex = 10;
+            this.cmbSectores.SelectedIndexChanged += new System.EventHandler(this.cmbSectores_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "DNI";
+            // 
+            // btnVerTodos
+            // 
+            this.btnVerTodos.Location = new System.Drawing.Point(12, 201);
+            this.btnVerTodos.Name = "btnVerTodos";
+            this.btnVerTodos.Size = new System.Drawing.Size(154, 47);
+            this.btnVerTodos.TabIndex = 8;
+            this.btnVerTodos.Text = "Ver todos";
+            this.btnVerTodos.UseVisualStyleBackColor = true;
+            this.btnVerTodos.Click += new System.EventHandler(this.btnVerTodos_Click);
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(9, 129);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(157, 47);
+            this.btnCargar.TabIndex = 7;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // dgvGrilla
+            // 
+            this.dgvGrilla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvGrilla.Location = new System.Drawing.Point(211, 12);
+            this.dgvGrilla.Name = "dgvGrilla";
+            this.dgvGrilla.Size = new System.Drawing.Size(577, 496);
+            this.dgvGrilla.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.btnDelete);
+            this.groupBox2.Controls.Add(this.cmbSectEliminar);
+            this.groupBox2.Controls.Add(this.txtDelete);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 355);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(180, 153);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Sector";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(12, 111);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(154, 27);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Eliminar";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // cmbSectEliminar
+            // 
+            this.cmbSectEliminar.FormattingEnabled = true;
+            this.cmbSectEliminar.Location = new System.Drawing.Point(12, 33);
+            this.cmbSectEliminar.Name = "cmbSectEliminar";
+            this.cmbSectEliminar.Size = new System.Drawing.Size(154, 21);
+            this.cmbSectEliminar.TabIndex = 15;
+            // 
+            // txtDelete
+            // 
+            this.txtDelete.Location = new System.Drawing.Point(12, 85);
+            this.txtDelete.Name = "txtDelete";
+            this.txtDelete.Size = new System.Drawing.Size(157, 20);
+            this.txtDelete.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "DNI";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 520);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.dgvGrilla);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Gestor de Mesas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGrilla)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnVerTodos;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.TextBox txtDNI;
+        private System.Windows.Forms.ComboBox cmbSectores;
+        private System.Windows.Forms.DataGridView dgvGrilla;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCap;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cmbSectEliminar;
+        private System.Windows.Forms.TextBox txtDelete;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ FinalLab1m2/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Collections.Specialized.BitVector32;
+
+namespace FinalLab1m2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //VARIABLES Y ARREGLOS GLOBALES
+        string[,] salon = new string[20, 10];
+        string[,] arriba = new string[10, 10];
+        string[,] living = new string[3, 2];
+        string[,] patio = new string[5, 5];
+
+        string[,] matAux; //matriz auxiliar, defino el tipo de dato pero no la dimension
+
+        int cap = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DefaultForm();
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            string dni = txtDNI.Text;
+            int index = cmbSectores.SelectedIndex;
+            matSelection(index);
+
+            //verifico que no exista el dni en ningun otro sector
+            if(controlDNI(salon,dni) && controlDNI(arriba, dni) && controlDNI(living, dni) && controlDNI(patio, dni))
+            {
+                dgvGrilla.Rows.Clear();
+                add(matAux, dni);
+                printGrid(matAux,true, cmbSectores.Text);
+                if (cap >= matAux.Length)
+                {
+                    MessageBox.Show("Capacidad superada", "Error");
+                }
+            }
+            else
+            {
+                MessageBox.Show("El DNI ingresado ya se encuentra en el sistema", "Error");
+            }
+        }
+
+        private void cmbSectores_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = cmbSectores.SelectedIndex;
+            matSelection(index);
+            dgvGrilla.Rows.Clear();
+            printGrid(matAux,true, cmbSectores.Text);
+        }
+
+        private void btnVerTodos_Click(object sender, EventArgs e)
+        {
+            dgvGrilla.Rows.Clear();
+            printGrid(salon, false, "Salon");
+            printGrid(arriba, false, "Arriba");
+            printGrid(living, false, "Living");
+            printGrid(patio, false, "Patio");
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            DefaultForm();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int index = cmbSectores.SelectedIndex;
+            string dni = txtDelete.Text;
+            bool control = true;
+            int f = 0;
+            int c = 0;
+            matSelection(index);
+
+            while(f < matAux.GetLength(0))
+            {
+                c = 0;
+                while(c < matAux.GetLength(1))
+                {
+                    if (matAux[f,c] == dni)
+                    {
+                        matAux[f,c] = null;
+                        control = false;
+                    }
+                    c++;
+                }
+                f++;
+            }
+
+            if (control)
+            {
+                MessageBox.Show("DNI no encontrado", "Error");
+            }
+            else
+            {
+                MessageBox.Show("Mesa liberada", "Confirmación");
+            }
+        
+            txtDelete.Text = string.Empty;
+            printGrid(matAux, true, cmbSectEliminar.Text);
+        }
+
+        public void printGrid(string[,] mat, bool clear, string sector)
+        {
+            cap = 0;
+         
+            if (clear)
+            {
+                dgvGrilla.Rows.Clear();
+            }
+            for (int f = 0; f < mat.GetLength(0); f++)
+            {
+                for(int c = 0; c < mat.GetLength(1); c++)
+                {
+                    if (mat[f,c] != null)
+                    {
+                        dgvGrilla.Rows.Add(mat[f, c], f+1, c+1, sector);
+                        cap++;
+                    }
+                }
+            }
+            lblCap.Text = cap + "/" + matAux.Length.ToString();
+        }
+
+        public void matSelection(int index)
+        {
+            //Le doy el valor de la matriz segun el sector a la matriz auxiliar
+            switch (index)
+            {
+                case 0:
+                    matAux = salon;
+                    break;
+                case 1:
+                    matAux = arriba;
+                    break;
+                case 2:
+                    matAux = living;
+                    break;
+                case 3:
+                    matAux = patio;
+                    break;
+            }
+        }
+
+        public void add(string[,] mat, string dni)
+        {
+            cap = 0;
+            bool control = true;
+            int c = 0;
+            int f = 0;
+            while ( f < mat.GetLength(0) && control)
+            {
+                c = 0;
+                while (c < mat.GetLength(1) && control)
+                {
+                    if (mat[f,c] == null)
+                    {
+                        if(cap < mat.Length && mat[f,c] == null)
+                        {
+                            mat[f, c] = dni;
+                            control = false;
+                            cap++;
+                        }
+                    }
+                    else
+                    {
+                        cap++;
+                    }
+                    c++;
+                }
+                f++;
+            }
+        }
+
+        static bool controlDNI(string[,] mat, string dni)
+        {
+            bool control = true;
+            int f = 0;
+            int c = 0;
+
+            while (f < mat.GetLength(0) && control)
+            {
+                c = 0;
+                while(c < mat.GetLength(1) && control)
+                {
+                    if (mat[f, 0] == dni)
+                    {
+                        control = false;
+                    }
+                    c++;
+                }
+                f++;
+            }
+            return control;
+        }
+        public void cleanMat(string[,] mat )
+        {
+            for(int f = 0; f < mat.GetLength(0); f++)
+            {
+                for(int c = 0; c < mat.GetLength(1); c++)
+                {
+                    mat[f,c] = null;
+                }
+            }
+        }
+
+        public void DefaultForm()
+        {
+            string[] sectores = { "Salon", "Arriba", "Living", "Patio" };
+            cmbSectores.Items.Clear();
+
+            foreach (string s in sectores)
+            {
+                cmbSectores.Items.Add(s);
+                cmbSectEliminar.Items.Add(s);
+            }
+
+            txtDNI.Text = string.Empty;
+            lblCap.Text = string.Empty;
+
+            //Reinicio el valor de las varibales y arreglos globales
+            cap = 0;
+            cleanMat(salon);
+            cleanMat(arriba);
+            cleanMat(living);
+            cleanMat(patio);
+
+
+            dgvGrilla.Rows.Clear();
+            dgvGrilla.Columns.Clear();
+
+            dgvGrilla.Columns.Add("col0", "DNI");
+            dgvGrilla.Columns.Add("col1", "FILA");
+            dgvGrilla.Columns.Add("col1", "MESA");
+            dgvGrilla.Columns.Add("col1", "SECTOR");
+        }
+    }
+}
